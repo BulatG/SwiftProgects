13c6789c35b5adbfcb910a98d33a110192a85408
Bulat <b.gabdullin@mail.ru>
2021-09-16 23:51:28 +0300

    this is my application

---
+++ MyInstagram/MyInstagram/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  MyInstagram
+//
+//  Created by Булат on 16.09.2021.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
